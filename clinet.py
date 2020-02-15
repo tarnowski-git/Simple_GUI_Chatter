a676243637d54ee1f61001402d3f8f01f850c2f2
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-02-16 00:35:44 +0100

    Add buffering and streaming data.

--- clinet.py
+++ clinet.py
@@ -1,21 +1,33 @@
 import socket
 
+HEADER_SIZE = 10
+
 # Create socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
-port = 444
+port = 999
 
 # connecting with server
 client_socket.connect((host, port))
 
-
-full_massage = ""
 while True:
-    # Receiving a maximum of 8 bytes / buffer for chunks
-    message = client_socket.recv(8)
-    if len(message) <= 0:
-        break
-    full_massage += message.decode("utf-8")
+    full_massage = ""
+    new_massege = True
+    while True:
+        # Receiving a maximum of 16 bytes / buffer for chunks
+        message = client_socket.recv(16)
+        if new_massege:
+            print(f"new message lenght: {message[:HEADER_SIZE]}")
+            message_lenght = len(message[: HEADER_SIZE])
+            new_massege = False
 
-print(full_massage)
+        full_massage += message.decode("utf-8")
+
+        if len(full_massage) - HEADER_SIZE == message_lenght:
+            print("full message recvd")
+            print(full_massage[10:])
+            new_massege = True
+            full_massage = ""
+
+    print(full_massage)
--- server.py
+++ server.py
@@ -1,28 +1,38 @@
 import socket
+import time
+
+HEADER_SIZE = 10
 
 # creating the socket object - ipv4, tcp ip
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # store the host IP address
 host = socket.gethostname()
-port = 444
+port = 999
 
 # binding host & port with socket
 server_socket.bind((host, port))
 
 # statring TCP listener of three contections
-server_socket.listen(3)
+server_socket.listen(5)
 
 
 while True:
     # starting the  from clinet
     client_socket, address = server_socket.accept()
-
     print(f"receved connection from {address} has been established!")
 
-    message = "Thank you for connecting to the server" + "\r\n"
+    message = "Welcome to the Server!"
+    # fixed lenth to the left for 10 characters
+    message = f"{len(message):<{HEADER_SIZE}}" + message
+    print(message)
 
     # sending information to the client socket
-    client_socket.send(bytes("Welcome to the server!", "utf-8"))
+    client_socket.send(bytes(message, "utf-8"))
 
-    client_socket.close()
+    while True:
+        time.sleep(3)
+        message = f"The time is! {time.time()}"
+        message = f"{len(message):<{HEADER_SIZE}}" + message
+        print(message)
+        client_socket.send(bytes(message, "utf-8"))
